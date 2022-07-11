27c95b3de70817cdd7098d2cafebe5134a043e9f
Miggy Eusebio <miggy.eusebio@gitkraken.com>
2022-07-11 14:25:22 -0700

    add gk graph with refs and commits

--- src/plus/webviews/graph/graphWebview.ts
+++ src/plus/webviews/graph/graphWebview.ts
@@ -16,7 +16,7 @@ export class GraphWebview extends WebviewWithConfigBase<State> {
 		super(container, 'gitlens.graph', 'graph.html', 'images/gitlens-icon.png', 'Graph', Commands.ShowGraphPage);
 	}
 
-	override async show(column: ViewColumn = ViewColumn.Beside): Promise<void> {
+	override async show(column: ViewColumn = ViewColumn.Active): Promise<void> {
 		if (!(await ensurePlusFeaturesEnabled())) return;
 		return super.show(column);
 	}
@@ -42,14 +42,76 @@ export class GraphWebview extends WebviewWithConfigBase<State> {
 			return undefined;
 		}
 
-		const log = await this.getLog(repo);
-		if (log?.commits === undefined) {
+		const [currentUser, log, remotes, tags, branches] = await Promise.all([
+			this.container.git.getCurrentUser(repo as string),
+			this.getLog(repo as string),
+			this.container.git.getRemotes(repo as string),
+			this.container.git.getTags(repo as string),
+			this.container.git.getBranches(repo as string),
+		]);
+
+        if (log == null || log?.commits === undefined) {
 			return undefined;
+        }
+
+		const name = currentUser?.name ? `${currentUser.name} (you)` : 'You';
+
+		const commitList: any[] = [];
+		console.log('zzz log', log)	;
+		console.log('zzz remotes', remotes);
+		console.log('zzz tags', tags);
+		console.log('zzz branches', branches);
+
+		for (const commit of log.commits.values()) {
+			// console.log('commitz', commit);
+			const commitBranch = branches.values.find(b => b.sha === commit.sha);
+			let branchInfo = {} as any;
+			if (commitBranch != null) {
+				branchInfo = {
+					remotes: [
+					{
+						name: commitBranch.name,
+						url: commitBranch.id
+					}
+					]
+				};
+				if (commitBranch.current) {
+					branchInfo.heads = [
+						{
+							name: commitBranch.name,
+							isCurrentHead: true
+						}
+					];
+				}
+			}
+			const commitTag = tags.values.find(t => t.sha === commit.sha);
+			let tagInfo = {} as any;
+			if (commitTag != null) {
+				tagInfo = { tags: [
+						{
+							name: commitTag.name,
+						}
+					]
+				};
+			}
+			commitList.push({
+				sha: commit.sha,
+				parents: commit.parents,
+				author: commit.author.name === 'You' ? name : commit.author.name,
+				date: commit.date.getTime(),
+				message: commit.message ?? commit.summary,
+				email: commit.author.email,
+				type: 'commit-node',
+				... branchInfo,
+				... tagInfo,
+			});
 		}
+
+		commitList.sort((a, b) => b.date - a.date);
 
 		return {
 			log: log,
-			commits: Array.from(log.commits.values()),
+			commits: commitList,
 		};
 	}
 
@@ -108,6 +170,7 @@ export class GraphWebview extends WebviewWithConfigBase<State> {
 							cursor: log.cursor,
 					  }
 					: undefined,
+			nonce: super.getCSPNonce()
 		};
 	}
 
@@ -117,11 +180,12 @@ export class GraphWebview extends WebviewWithConfigBase<State> {
 }
 
 function formatCommits(commits: GitCommit[]): GitCommit[] {
-	return commits.map(({ sha, author, message }) => ({
-		sha: sha,
-		author: author,
-		message: message,
-	}));
+	return commits;
+	// return commits.map(({ sha, author, message }) => ({
+	// 	sha: sha,
+	// 	author: author,
+	// 	message: message,
+	// }));
 }
 
 function formatRepositories(repositories: Repository[]): Repository[] {
---
+++ src/webviews/apps/plus/graph/GKGraph.tsx
@@ -0,0 +1,117 @@
+import GraphContainer, { GraphRow } from '@axosoft/gitkraken-components/lib/components/graph/GraphContainer';
+import * as React from 'react';
+// import { TextDocument, Uri, workspace } from 'vscode';
+
+// TODO: remove mockGraphRows constant once we get fixed GITLENS-203
+const mockGraphRows: GraphRow[] = [
+  {
+    'sha': '1511222233334447',
+    'parents': ['1411222233334447', '1211222233334444'],
+    'author': 'Miggy Eusebio',
+    'email': 'miggy.eusebio@gitkraken.com',
+    'date': 1655151460000,
+    'message': 'Child commit',
+    'type': 'commit-node'
+  },
+  {
+    'sha': '1411222233334447',
+    'parents': ['1311222233334445'],
+    'author': 'Miggy Eusebio',
+    'email': 'miggy.eusebio@gitkraken.com',
+    'date': 1655065060000,
+    'message': 'Third commit',
+    'type': 'commit-node'
+  },
+  {
+    'sha': '1311222233334445',
+    'parents': ['1211222233334444'],
+    'author': 'Eric Follana',
+    'email': 'eric.follana@gitkraken.com',
+    'date': 1654978660000,
+    'message': 'Second commit',
+    'type': 'commit-node'
+  },
+  {
+    'sha': '1211222233334444',
+    'parents': [],
+    'author': 'Ramin Tadayon',
+    'email': 'ramin.tadayon@gitkraken.com',
+    'date': 1654892260000,
+    'message': 'Initial commit',
+    'type': 'commit-node'
+  }
+];
+
+interface GKProps {
+  extensionUri?: any;
+  graphRows?: GraphRow[];
+  repo?: string;
+  nonce?: string;
+}
+
+interface GKState {
+  rows?: GraphRow[];
+}
+
+export class GKGraph extends React.Component<GKProps, GKState> {
+  /**
+   * JIRA task GITLENS-203:
+   * Commented those lines because React component LifeCycle is not working properly
+   * because we are using "ReactDOMServer.renderToString" instead of "ReactDOM.render"
+   * in "gk-webview-panel.tsx" file. Done in that way because this project was an example
+   * but this is not the correct way.
+   *
+   * This need to be fixed as our lib component won't be able to be displayed and work properly.
+   *
+   * TODO: We need to use "ReactDOM.render" instead. For that, we will need to modify this
+   * project to build a new .js (that will do the "ReactDOM.render" call instead) to be
+   * included by using an script tag in "gk-webview-panel.tsx".
+   *
+   */
+  constructor(props: GKProps) {
+    super(props);
+
+    this.state = {
+      rows: []
+    };
+  }
+
+  override componentDidMount() {
+    // const {
+    //   // extensionUri
+    // } = this.props;
+
+    // Load mocks for testing the Graph
+
+    // const uri: Uri = vscode.Uri.joinPath(props.extensionUri, 'src', 'mocks', 'rows', 'simple-graph-rows-mock.json');
+    // const uri: Uri = vscode.Uri.joinPath(props.extensionUri, 'src', 'mocks', 'rows', 'graph-rows-mock-with-WIP.json');
+    // if (extensionUri != null) {
+    //   const uri: Uri = Uri.joinPath(extensionUri, 'src', 'mocks', 'rows', 'graph-rows-mock.json');
+    //   // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    //   workspace.openTextDocument(uri).then(
+    //     (document: TextDocument) => {
+    //       const jsonTxt: string = document.getText();
+    //       this.setState({ rows: JSON.parse(jsonTxt) });
+    //     }
+    //   );
+    // }
+  }
+
+  override render() {
+    const {
+      graphRows,
+      repo,
+      nonce
+    } = this.props;
+    return (
+      <div className="GKGraph">
+        <h2 >{repo}</h2>
+        <GraphContainer
+          graphRows={(graphRows != null)? graphRows : mockGraphRows}
+          useAuthorInitialsForAvatars={false}
+          nonce={nonce}
+        />
+      </div>
+    );
+  }
+}
